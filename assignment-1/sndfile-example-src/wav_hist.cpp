fd6712d5a377e29226740d7f274c7befba42ad2b
Diogo Jesus <diogopjesus@ua.pt>
2022-10-16 00:02:15 +0100

    Update wav_hist to write to files the avg and diff results

--- assignment-1/sndfile-example-src/wav_hist.cpp
+++ assignment-1/sndfile-example-src/wav_hist.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <sndfile.hh>
+#include <string.h>
 #include "wav_hist.h"
 
 using namespace std;
@@ -10,7 +11,7 @@ constexpr size_t FRAMES_BUFFER_SIZE = 65536; // Buffer for reading frames
 int main(int argc, char *argv[]) {
 
 	if(argc < 3) {
-		cerr << "Usage: " << argv[0] << " <input file> <channel>\n";
+		cerr << "Usage: " << argv[0] << " [--store-histograms] <input file> <channel>\n";
 		return 1;
 	}
 
@@ -36,6 +37,9 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
+	// Check if flag "--store-histograms" was used
+	bool store_histograms = argc > 3 && (strcmp(argv[argc-3], "--store-histograms") == 0);
+
 	size_t nFrames;
 	vector<short> samples(FRAMES_BUFFER_SIZE * sndFile.channels());
 	WAVHist hist { sndFile };
@@ -44,8 +48,7 @@ int main(int argc, char *argv[]) {
 		hist.update(samples);
 	}
 
-	hist.dump(channel);
+	hist.dump(channel, store_histograms);
 
 	return 0;
 }
-
--- assignment-1/sndfile-example-src/wav_hist.h
+++ assignment-1/sndfile-example-src/wav_hist.h
@@ -7,10 +7,13 @@
 #include <sndfile.hh>
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 
 class WAVHist {
   private:
 	std::vector<std::map<short, size_t>> counts;
+	std::map<short, size_t> avgcounts;
+	std::map<short, size_t> diffcounts;
 
   public:
 	WAVHist(const SndfileHandle& sfh) {
@@ -18,29 +21,57 @@ class WAVHist {
 	}
 
 	void update(const std::vector<short>& samples) {
-		
-		size_t n { };
-		short val = 0;
-		for(auto s : samples){
-			counts[n++ % 2][s]++;
-			if(n % 2 != 0){
-				val = s;
-			}
-			if(n % 2 == 0){
-				counts[2][(val+s)/2]++;
-				counts[3][(val-s)/2]++;
+		for(size_t i=0; i < samples.size(); i++) {
+			counts[i % 2][samples[i]]++;
+			/*
+				In samples:
+					- even indexes means left channel
+					- odd indexes means right channel
+				When index is odd, calculate the average and the difference
+				of samples for the left and right channels. Store the results in
+				maps representing the histograms of the calculations.
+			*/
+			if(i % 2 != 0){
+				avgcounts[(samples[i-1] + samples[i])/2]++;
+				diffcounts[(samples[i-1] - samples[i])/2]++;
 			}
 		}
 	}
 
-	void dump(const size_t channel) const {
-		std::ofstream myfile;
-		myfile.open ("hist.txt");
-		for(auto [value, counter] : counts[channel]){
-			myfile << value << ',' << counter << '\n';
-			std::cout << value << '\t' << counter << '\n';
+	// NOTE: use the extension ".dat" so the files can be easily ignored by .gitignore
+	void dump(const size_t channel, const bool store_histograms = false) const {
+		// Output normal usage, when flag to store histograms is not used
+		if(!store_histograms) {
+			for(auto [value, counter] : counts[channel])
+				std::cout << value << '\t' << counter << '\n';
+			return;
 		}
-		myfile.close();
+
+		std::string hist_path = "../plots/histograms/";
+		// Create directory (if exists, does nothing)
+		std::filesystem::create_directory(hist_path);
+
+		// Store histogram of the requested channel
+		std::ofstream outfile;
+		outfile.open(hist_path + "hist_channel_" + std::to_string(channel) + ".dat");		
+		for(auto [value, counter] : counts[channel]){
+			outfile << value << ',' << counter << '\n';
+		}
+		outfile.close();
+
+		// Store histogram of the average of the channels
+		outfile.open(hist_path + "hist_avg.dat");
+		for(auto [value, counter] : avgcounts) {
+			outfile << value << ',' << counter << '\n';
+		}
+		outfile.close();
+
+		// Store histogram of the difference of the channels
+		outfile.open(hist_path + "hist_diff.dat");
+		for(auto [value, counter] : diffcounts) {
+			outfile << value << ',' << counter << '\n';
+		}
+		outfile.close();
 	}
 };
 
