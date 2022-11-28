a67e0ac4465c55ed94e5ecd8f5bfe48e75ab194f
pedrocjdpereira <pedrocjdpereira@ua.pt>
2022-11-28 00:57:58 +0000

    audio codec ainda não funcional

---
+++ assignment-2/laudiocodec-src/bitstream.h
@@ -0,0 +1,101 @@
+#ifndef BITSTREAM_H
+#define BITSTREAM_H
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+constexpr unsigned int BUFFER_SIZE = 8;
+
+class BitStream {
+	private:
+		fstream stream;
+		char buffer;
+		int pointer;
+		int mode;
+		long long size;
+	
+	public:
+		BitStream(){}
+		
+		BitStream(const char *fname, char mode) {		
+			if(mode == 'w')
+				stream = fstream(fname, ios::binary | ios::out);
+			else if (mode == 'r')
+				stream = fstream(fname, ios::binary | ios::in);
+			else
+				throw runtime_error("Invalid opening mode for file!");
+			
+			if(!stream.is_open())
+				throw runtime_error("Could not open given file!");
+			
+			BitStream::mode = (mode == 'w') ? 1 : 0;
+			buffer = 0;
+			pointer = (mode == 'w') ? 8 : 0;
+
+			// get length of file in bytes
+			stream.seekg(0, stream.end);
+			size = stream.tellg();
+			stream.seekg(0, stream.beg);
+		}
+
+		char readBit() {
+			if(mode != 0)
+				throw runtime_error("File is not open on read mode!");
+			
+			// populate buffer if empty
+			if(--pointer == -1) {
+				stream.read(&buffer, 1);
+				pointer = 7;
+			}
+
+			char res = (buffer >> pointer) & 0x01;
+
+			return res;
+		}
+
+		void readNBits(char *bits, int n) {
+			if(mode != 0)
+				throw runtime_error("File is not open on read mode!");
+			
+			for(int i = 0; i < n; i++)
+				bits[i] = readBit();
+		}
+
+		void writeBit(char bit) {
+			if(mode != 1)
+				throw runtime_error("File is not open on write mode!");
+
+			// If last bit in buffer is being populated, empty buffer to file
+			if(--pointer == 0) {
+				buffer |= (bit & 0x01);
+				stream.write(&buffer, 1);
+				pointer = 8;
+				buffer = 0; // clean buffer to prevent writing wrong bits if file is closed before EOF
+			} else
+				buffer |= (bit & 0x01) << pointer;
+		}
+
+		void writeNBits(char *bits, int n) {
+			if(mode != 1)
+				throw runtime_error("File is not open on write mode!");
+			
+			for(int i = 0; i < n; i++)
+				writeBit(bits[i]);
+		}
+
+		long long byteSize() {
+			return size;
+		}
+
+		void close() {
+			// empty buffer to file if on writing mode and buffer not empty
+			if(mode == 1 && pointer != 8)
+				stream.write(&buffer, 1);
+
+			stream.close();
+		}
+};
+
+#endif
