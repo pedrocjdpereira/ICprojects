9bc3d0583956acc0d917c6768143d5a80dc14d31
Diogo Jesus <diogopjesus@ua.pt>
2022-11-29 17:09:19 +0000

    Add comments to img_copy

--- assignment-2/imgmanipulation-src/img_cpy.cpp
+++ assignment-2/imgmanipulation-src/img_cpy.cpp
@@ -1,28 +1,38 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
+using namespace std;
 
 int main(int argc, char *argv[]) {
-
+	/* Verify arguments */
 	if(argc < 3) {
-		std::cerr << "Usage: img_cpy <original file> <copy file> \n";
+		cerr << "Usage: img_cpy <original file> <copy file> \n";
 		return 1;
 	}
 
-	Mat ogimage = cv::imread(argv[argc - 2]);
-	if (ogimage.empty())
-	{
-		std::cerr << "!!! Failed imread(): image not found";
+	/* Read image passed as argument */
+	Mat original = imread(argv[argc - 2]);
+	if (original.empty()) {
+		cerr << "Failed imread(): image not found";
 		return 1;
 	}
-	Mat imagecopy(ogimage.rows, ogimage.cols, ogimage.type());
 
-	for(int i = 0; i < ogimage.rows; i++){
-		for(int j = 0; j < ogimage.cols; j++){
-			Vec3b pixel = Vec3b(ogimage.ptr<Vec3b>(i)[j][0], ogimage.ptr<Vec3b>(i)[j][1], ogimage.ptr<Vec3b>(i)[j][2]);
+	/* Create copy image object */
+	Mat imagecopy(original.rows, original.cols, original.type());
+	
+	/* Copy original image pixel by pixel */
+	for(int i = 0; i < original.rows; i++){
+		for(int j = 0; j < original.cols; j++){
+			Vec3b pixel = Vec3b(original.ptr<Vec3b>(i)[j][0], original.ptr<Vec3b>(i)[j][1], original.ptr<Vec3b>(i)[j][2]);
 			imagecopy.ptr<Vec3b>(i)[j] = pixel;
 		}
 	}
 
-	imwrite(argv[argc - 1], imagecopy);
+	/* Write copied image to file */
+	if(!imwrite(argv[argc - 1], imagecopy)) {
+		cerr << "Failed to write image to file!\n";
+		return -1;
+	}
+
+	return 0;
 }
